fd958a890bcc66f4eb829194499b2adb3a83b028
brechtopstaele <brecht.opstaele@hotmail.com>
2021-11-12 14:52:13 +0100

    Added classes for Gson

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.io.IOException;
+
 public class Main {
   public static void main(String[] args){
-    System.out.println("Hello World");
+    try {
+      Input input = JsonReader.inlezen();
+      System.out.println(input);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
   }
 }
