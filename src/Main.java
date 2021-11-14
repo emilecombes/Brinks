3e8b6cdd4c2f2e957a627421fce9b4d20f4b2fa0
ToonKeymeulen <toon.keymeulen@hotmail.com>
2021-11-14 15:29:55 +0100

    assigned every customer to a day

--- src/Main.java
+++ src/Main.java
@@ -13,19 +13,34 @@ public class Main {
 
 
       //Creeeren van array met set customers to server
-      Set<Customer>[] daysWithCustToServe = new Set[input.getDynamic_customers().get(0).getTime_windows().size()];
-      for (int i=0 ; i<daysWithCustToServe.length-1;i++) {
+      Set<Customer>[] daysWithCustToServe = new Set[input.getNum_periods()];
+      for (int i=0 ; i<daysWithCustToServe.length;i++) {
         daysWithCustToServe[i] = new HashSet<>();
       }
 
-      //TOEVOEGEN VAN DYNAMISCHE KLANTEN AAN DAG(MAX AANTAL OPEENVOLGENDE DAGEN GEBRUIKEN)
+      //TOEVOEGEN VAN DYNAMISCHE KLANTEN AAN DAG
       for (DynamicCustomer dc : input.getDynamic_customers()) {
-        //daysWithCustToServe[]
+        //TOEVOEGEN EERSTE BEZOEK
+        daysWithCustToServe[dc.getDyn_fvisit()].add(dc);
+
+        //TOEVOEGEN ANDERE BEZOEKEN(MAX AANTAL OPEENVOLGENDE DAGEN GEBRUIKEN)
+        for (int i = dc.getDyn_fvisit(); i<input.getNum_periods();i++) {
+
+          if ((i-dc.getDyn_fvisit())%(dc.getDyn_nvisit()+1)==0) {
+            daysWithCustToServe[i].add(dc);
+          }
+        }
       }
 
 
 
       //TOEVOEGEN VAN STATISCHD KLANTEN AAN DAG(VIA PARAMETERS)
+      for (StaticCustomer sc : input.getFixed_customers()) {
+        for (int i = 0 ; i <sc.getFixed_pattern().size();i++) {
+          daysWithCustToServe[sc.getFixed_pattern().get(i)].add(sc);
+        }
+      }
+
 
 
 
