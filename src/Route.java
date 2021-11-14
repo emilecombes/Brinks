538a55eee9f3554d92b683291f464559807a0d1f
ToonKeymeulen <toon.keymeulen@hotmail.com>
2021-11-14 12:06:40 +0100

    added out folder

--- src/Route.java
+++ src/Route.java
@@ -17,4 +17,29 @@ public class Route {
   public boolean isFeasibleWith(Customer c){
     return false;
   }
+
+    //EDT (ci) = max(EDT (ci−1) + T T (ci−1, ci), StartT Wi) + STi
+
+/*
+    public int[] calculateEarliestDepartureTime() {
+      int[] EDT = new int[route.size()];
+      EDT[0]=0;
+      for (int i = 1; i<EDT.length-2;i++) {
+          EDT[i]= Math.max(EDT[i-1]+distance[i-1][i], route.get(i).time_windows.get(dat)) + route.get(i).getDuration();
+      }
+      return earliestDepartureTimes;
+    }
+
+
+    //LST (ci) = min(LST (ci+1) − T T (ci+1, ci) − STi, EndT Wi)
+
+    public int[] calculateLatestStartingTime() {
+      int [] latestStartingTimes = new int[route.size()];
+
+      return solution;
+    }
+
+
+*/
+
 }
